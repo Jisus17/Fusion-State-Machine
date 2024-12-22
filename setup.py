375ba62b6a7b715e5569c7894aa84344814f387a
neo_dkt <56320001+Jisus17@users.noreply.github.com>
2024-12-22 21:44:44 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup, find_packages
 from pathlib import Path
-this_directory = Path(__file__).parent
-long_description = (this_directory / "README.md").read_text()
+#this_directory = Path(__file__).parent
+#long_description = (this_directory / "README.md").read_text()
 
 
 setup(
     name="Fusion-State-Machine",
     version="0.1.3",
     description="Utility to turn your class into a state machine",
-    long_description=long_description,
-    long_description_content_type='text/markdown'
+    #long_description=long_description,
+    #long_description_content_type='text/markdown'
     url="https://github.com/Jisus17/Fusion-State-Machine",
     author="Original Author Dhruv Agarwal forked by Jisus17",
     author_email="dhruv.agarwal@shuttl.com",
